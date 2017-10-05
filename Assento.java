55d576863a6cd93d3eac332630a5499fa9135152
igorcs08 <31697510+igorcs08@users.noreply.github.com>
2017-10-05 16:19:30 -0300

    Update Assento.java

--- Assento.java
+++ Assento.java
@@ -1,6 +1,19 @@
 public class Assento{
 	private int fila;
 	private char coluna;
+	
+	public void setFila(int fila){
+		this.fila = fila;
+	}
+	public int getFila(){
+		return fila;
+	}
+	public void setColuna(int coluna){
+		this.coluna = coluna;
+	}
+	public int getColuna(){
+		return coluna;
+	}
 
 	public boolean vago(){
 		
